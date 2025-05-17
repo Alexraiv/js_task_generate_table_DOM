e3297db071caa3f6dcc1f96294245cd645951ffc
Oleksandr Voronkin <94705289+Alexraiv@users.noreply.github.com>
2025-05-17 15:02:36 +0300

    fix

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -364,14 +364,12 @@ people.forEach((person) => {
   const row = document.createElement('tr');
 
   row.innerHTML = `
-  <tr>
     <td>${person.name}</td>
     <td>${person.sex === 'm' ? 'Male' : 'Female'}</td>
     <td>${person.born}</td>
     <td>${person.died}</td>
     <td>${person.died - person.born}</td>
     <td>${Math.ceil(person.died / 100)}</td>
-  </tr>
-  `;
+ `;
   table.appendChild(row);
 });
